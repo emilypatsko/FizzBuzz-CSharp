2449a7b7288981b2d2c857bb0c00f2b3ca58c433
Emily Patsko <Emily.Patsko@softwire.com>
2021-02-22 11:43:44 +0000

    Reorganized the code making use of lists.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FizzBuzz_C_
 {
@@ -7,18 +8,25 @@ namespace FizzBuzz_C_
         static void Main(string[] args)
         {
             int length = 100;
+            List<string> thingsToPrint = new List<string> ();
+            
             for (var i = 0; i < length; i++) {
-                int num = i + 1;
-                if ((num % 3 == 0) && (num % 5 == 0)) {
-                    Console.WriteLine("FizzBuzz");
-                } else if (num % 3 == 0) {
-                    Console.WriteLine("Fizz");
-                } else if (num % 5 == 0 ) {
-                    Console.WriteLine("Buzz");
-                } else {
-                    Console.WriteLine(num);
-                }                
+                int num = i + 1;    
+                if ((num % 3 != 0) && (num % 5 != 0)) {
+                    thingsToPrint.Add(num.ToString());
+                } else {       
+                    thingsToPrint.Add("");  
+
+                    if (num % 3 == 0) {
+                        thingsToPrint[i] = "Fizz";
+                    }
+                    if (num % 5 == 0) {
+                        thingsToPrint[i] += "Buzz";
+                    }
+                }         
             }
+
+            thingsToPrint.ForEach(Console.WriteLine);
         }
     }
 }

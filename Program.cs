c8c1765ff6dbf0d51a5d9594b0483bead534230a
Emily Patsko <Emily.Patsko@softwire.com>
2021-02-22 12:28:04 +0000

    For multiples of 13, print 'Fezz' instead of the number. If a multiple of other numbers, print Fezz before first B (or at end if there are none). Print even when Bong present.

--- Program.cs
+++ Program.cs
@@ -7,19 +7,19 @@ namespace FizzBuzz_C_
     {
         static void Main(string[] args)
         {
-            int length = 106;
+            int length = 196;
             List<string> thingsToPrint = new List<string> ();
             
             for (var i = 0; i < length; i++) {
                 int num = i + 1;    
-                if ((num % 3 != 0) && (num % 5 != 0) && (num % 7 != 0) && (num % 11 != 0)) {
+                if ((num % 3 != 0) && (num % 5 != 0) && (num % 7 != 0) && (num % 11 != 0) && (num % 13 != 0)) {
                     thingsToPrint.Add(num.ToString());
                 } else {       
                     thingsToPrint.Add("");  
 
                     if (num % 3 == 0) {
                         thingsToPrint[i] += "Fizz";
-                    }
+                    }                    
                     if (num % 5 == 0) {
                         thingsToPrint[i] += "Buzz";
                     }
@@ -28,6 +28,14 @@ namespace FizzBuzz_C_
                     }
                     if (num % 11 == 0) {
                         thingsToPrint[i] = "Bong";
+                    }
+                    if (num % 13 == 0) {
+                        int bIndex = thingsToPrint[i].IndexOf("B");
+                        if (bIndex > -1) {
+                            thingsToPrint[i] = thingsToPrint[i].Substring(0, bIndex) + "Fezz" + thingsToPrint[i].Substring(bIndex);
+                        } else {
+                            thingsToPrint[i] += "Fezz";
+                        }                      
                     }
                 }         
             }
